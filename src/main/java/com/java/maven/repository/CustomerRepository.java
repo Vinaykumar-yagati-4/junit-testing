41a75d3b847655e191e99a251098b864d6e3347d
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-08-26 08:54:45 +0530

    feat :Add CustomerRepository

---
+++ src/main/java/com/java/maven/repository/CustomerRepository.java
@@ -0,0 +1,26 @@
+package com.java.maven.repository;
+
+import com.java.maven.model.Customer;
+import java.util.List;
+
+public class CustomerRepository {
+
+    List<Customer> customers;
+    public CustomerRepository(List<Customer> customers) {
+        this.customers = customers;
+    }
+    /*
+    without exceptions
+    write CRUD methods
+    Optional<Customer> findById(int id);
+    List<Customer> findAll();
+    Customer save(Customer customer);
+    Customer update(Customer customer);
+    void deleteById(int id);
+    List<String> getCustomerNames();
+    Optional<Customer> findCustomerByHighestBalance();
+    Optional<Customer> findCustomerByLowestBalance();
+    List<Customer> sortCustomersByNameAsc();
+    List<Customer> sortCustomersByNameDesc();
+     */
+}
