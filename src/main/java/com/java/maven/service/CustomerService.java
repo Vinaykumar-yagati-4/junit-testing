5a38cdef5b94108bc3689d9ff0b63665a6abd1c1
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-01 22:29:08 +0530

    feat :Add CustomerService

--- src/main/java/com/java/maven/service/CustomerService.java
+++ src/main/java/com/java/maven/service/CustomerService.java
@@ -14,6 +14,7 @@ public class CustomerService {
     }
 
     public Customer addCustomer(Customer customer) {
+
         boolean exists = customers.stream()
                 .anyMatch(c -> c.getId() == customer.getId());
 
